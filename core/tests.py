358aaabb1ea069a68f1587f655bde7987f18e8bc
Luiz Paulo Coutinho <coutinholps@gmail.com>
2024-08-23 09:27:00 -0300

    Home and Contact pages

--- core/tests.py
+++ core/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from .models import CustomUser  # Importando o modelo CustomUser
+from .models import Congregation, CustomUser
 
 
 class AdminLoginTest(TestCase):
@@ -33,3 +33,64 @@ class AdminLoginTest(TestCase):
         # Tenta acessar a página de admin
         response = self.client.get(reverse("admin:index"))
         self.assertRedirects(response, "/admin/login/?next=/admin/")
+
+
+class HomePageTests(TestCase):
+    def setUp(self):
+        # Criar um objeto Congregation para ser usado no teste
+        self.congregation = Congregation.objects.create(
+            name="My Church", address="Rua dos Bobos, 0"
+        )
+
+    def test_home_page_status_code(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_contains_header_and_footer(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "<header>")
+        self.assertContains(response, "</header>")
+        self.assertContains(response, "<footer>")
+        self.assertContains(response, "</footer>")
+
+    def test_home_page_displays_congregation_info(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, self.congregation.name)
+        self.assertContains(response, self.congregation.address)
+
+
+class ContactPageTests(TestCase):
+    def test_contact_page_status_code(self):
+        response = self.client.get(reverse("contact"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_contact_page_contains_correct_content(self):
+        response = self.client.get(reverse("contact"))
+        self.assertContains(response, "<h2>Contact Me</h2>")
+        self.assertContains(response, "coutinholps@gmail.com")
+        self.assertContains(response, "https://wa.me/5521964781930")
+        self.assertContains(response, "https://www.linkedin.com/in/luizpaulocoutinho/")
+
+    def test_contact_page_contains_clickable_email(self):
+        response = self.client.get(reverse("contact"))
+        self.assertContains(
+            response, '<a href="mailto:coutinholps@gmail.com">coutinholps@gmail.com</a>'
+        )
+
+    def test_contact_page_contains_clickable_phone(self):
+        response = self.client.get(reverse("contact"))
+        self.assertContains(
+            response, '<a href="https://wa.me/5521964781930">+55 21 964 781 930</a>'
+        )
+
+    def test_contact_page_contains_clickable_linkedin(self):
+        response = self.client.get(reverse("contact"))
+        self.assertContains(
+            response,
+            '<a href="https://www.linkedin.com/in/luizpaulocoutinho/" target="_blank" '
+            'rel="noopener noreferrer">Luiz Paulo Coutinho</a>',
+        )
+
+    def test_contact_page_uses_base_template(self):
+        response = self.client.get(reverse("contact"))
+        self.assertTemplateUsed(response, "base.html")
--- core/views.py
+++ core/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
-_ = render
+from .models import Congregation
+
+
+def home(request):
+    congregation = Congregation.objects.first()  # Buscar a primeira congregação
+    return render(request, "home.html", {"congregation": congregation})
+
+
+def contact_page(request):
+    return render(request, "contact.html")
